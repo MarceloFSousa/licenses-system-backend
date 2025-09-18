b15445379ecf84f1e015ba16b1d5e72786620213
Marcelo <omarcelo.0101@gmail.com>
2025-09-18 19:32:45 -0300

    initial commit 3

--- Domain/Repository/LicensesContextFactory.cs
+++ Domain/Repository/LicensesContextFactory.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
 
 namespace Domain.Repository
 {
-    internal class LicensesContextFactory
+    public class LicensesContextFactory : IDesignTimeDbContextFactory<LicensesContext>
     {
+        public LicensesContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<LicensesContext>();
+
+            optionsBuilder.UseNpgsql(
+                "Server=localhost;Database=LicensesDb;User Id=sa;Password=YourPassword;TrustServerCertificate=True");
+
+            return new LicensesContext(optionsBuilder.Options);
+            Env.Load();
+
+            var connectionString = Environment.GetEnvironmentVariable("CONNECTION_STRING");
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                throw new InvalidOperationException("CONNECTION_STRING env var is not set.");
+            }
+
+            var optionsBuilder = new DbContextOptionsBuilder<LicensesContext>();
+            optionsBuilder.UseNpgsql(connectionString);
+        }
     }
-}
